9a90ff85d883c6efb36761da81b4ced9605500fb
jiikko <n905i.1214@gmail.com>
2016-09-02 10:43:54 +0900

    fix

--- README.md
+++ README.md
@@ -16,11 +16,6 @@ Or install it yourself as:
     $ gem install sugoi_activerecord_cache
 
 ## Usage
-* cache type
-  * key value
-  * list of activerecord instance
-
-### key value
 ```ruby
 class SystemProperty < ActiveRecord::Base
   include SugoiActiveRecordCache::Record
@@ -39,8 +34,6 @@ SystemProperty.find_by_from_cache(key: 'synced_at') # => '2011-01-01'
 SystemProperty.find_by_from_cache(key: 'site_name') # => 'アンテナサイト'
 SystemProperty.cached
 ```
-
-### list of activerecord instance
 ```ruby
 class ChildAge < ActiveRecord::Base
   include SugoiActiveRecordCache::Record
@@ -59,12 +52,6 @@ ChildAge.find_by_from_cache(name: 9)
 ChildAge.find_by_from_cache(description: 'ですです')
 ChildAge.cached
 ```
-
-## Development
-
-After checking out the repo, run `bin/setup` to install dependencies. Then, run `rake spec` to run the tests. You can also run `bin/console` for an interactive prompt that will allow you to experiment.
-
-To install this gem onto your local machine, run `bundle exec rake install`. To release a new version, update the version number in `version.rb`, and then run `bundle exec rake release`, which will create a git tag for the version, push git commits and tags, and push the `.gem` file to [rubygems.org](https://rubygems.org).
 
 ## Contributing
 
--- lib/sugoi_activerecord_cache.rb
+++ lib/sugoi_activerecord_cache.rb
@@ -1,9 +1,9 @@
 require "sugoi_activerecord_cache/version"
 
 module SugoiActiverecordCache
-  module Base
+  module Record
     def cache_key
-      self.table_name
+      "sugoi_activerecord_cache/#{self.table_name}"
     end
 
     def sugoi_activerecord_cache(method_name, options)
@@ -11,8 +11,6 @@ module SugoiActiverecordCache
       @expire_in = options[:expire_in]
       @cache_key = options[:cache_key] || cache_key
     end
-
-    private
 
     def fetch
       Rails.cache.fetch(@cache_key, expire_in: @expire_in) do
@@ -24,25 +22,35 @@ module SugoiActiverecordCache
         end
       end
     end
-  end
-
-  module Record
-    include Base
 
     def find_by_from_cache(params)
+      key, value = get_key_value(params)
+      find_from_cache(key, value) || self.find_by(key => value)
+    end
+
+    def find_by_from_cache!(params)
+      key, value = get_key_value(params)
+      find_from_cache(key, value) || self.find_by!(key => value)
+    end
+
+    def cached
+      fetch
+    end
+
+    private
+
+    def get_key_value(params)
       raise('don\'t support multi keys arg') if params.keys.size != 1
-      key = params.keys.first
-      value = params.values.first
+      [params.keys.first, params.values.first]
+    end
+
+    def find_from_cache(key, value)
       fetch.each do |cached_record|
         if cached_record.public_send(key) == value
           return cached_record
         end
       end
       return nil
-    end
-
-    def cached
-      fetch
     end
   end
 end
